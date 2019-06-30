f91ae2a22eeeaf4ae0eafd8f01f76624c890ae42
Justin Ho <justinho515@gmail.com>
2019-06-29 21:08:48 -0700

    Create book to add to reading list

--- books/urls.py
+++ books/urls.py
@@ -3,8 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index),
-    # path("create/", views.create_book),
-    # path("delete/", views.delete_book),
-    # path("edit/", views.edit_book),
+    path("", views.index, name="index"),
+    path("create/", views.create, name="create"),
+    # path("delete/", views.delete, name="delete"),
+    # path("update/", views.update, name="update"),
 ]
--- books/views.py
+++ books/views.py
@@ -4,34 +4,46 @@ from .forms import BookForm
 from .models import Book
 
 # Create your views here.
-# def create_book(request):
-#     """ Create a book to add to a user's book list. """
-#     if request.method == 'POST':
-#         form = BookForm(request.POST)
-#         if form.is_valid():
-#             try:
-#                 form.save()
-#                 return redirect('/show')
-#             except:
-#                 pass
-#         else:
-#             form = BookForm()
-#         return render(request, 'templates/create.html', {'form': form})
+def create(request):
+    """ Create a book to add to a user's book list. """
+    if request.method == 'POST':
+        form = BookForm(request.POST)
+        if form.is_valid():
+            try:
+                # Create Book object to store in db.
+                book = Book()
+                book.title = form.cleaned_data["title"]
+                book.author = form.cleaned_data["author"]
+                book.genre = form.cleaned_data["genre"]
+
+                print('book title: ' + book.title)
+
+                # Save the new Book object in the db.
+                book.save()
+                return redirect('')
+            except:
+                pass
+        else:
+            form = BookForm()
+        return render(request, 'books/create.html', {'form': form})
+    else:
+        form = BookForm()
+        return render(request, 'books/create.html', {'form': form})
 
 def index(request):
-    """ Show the user a his/her list of books. """
+    """ Show the user a his/her list of books. """    
     context = {
         "books": Book.objects.all()
     }    
     return render(request, 'books/index.html', context)
 
-# def edit_book(request, id):
+# def update(request, id):
 #     """ Edit details of a user's book in their book list. """
 #     book = Book.objects.get(id=id)
 #     return render(request, 'templates/edit.html', {'book': book})
 
-# def delete_book(request, id):
+# def delete(request, id):
 #     """ Remove a book from a user's book list. """
 #     book = Book.objects.get(id=id)
 #     book.delete()
-#     return redirect('/')
+#     return redirect('')
