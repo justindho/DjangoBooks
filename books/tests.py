8c129a976e528a70ff4d7f5f5599bb45619de170
Justin Ho <justinho515@gmail.com>
2019-07-03 11:28:50 -0700

    Add update book information functionality

--- books/tests.py
+++ books/tests.py
@@ -1,16 +1,21 @@
 from django.test import TestCase, Client
+from django.urls import reverse
+from selenium import webdriver
 
 from .models import Book
 
 class BookTestCase(TestCase):
+
     def setUp(self):
         Book.objects.create(title="book1", author="author1", genre="genre1")
         Book.objects.create(title="book2", author="author2", genre="genre2")
+        self.c = Client(enforce_csrf_checks=True)
+        self.driver = webdriver.Firefox()
 
     # MODELS
     def test_book_creation(self):
         book1 = Book.objects.get(id=1)
-        expected_book1_title = f'{book1.title}'        
+        expected_book1_title = f'{book1.title}'
         self.assertEquals(expected_book1_title, 'book1')
         book2 = Book.objects.get(id=2)
         expected_book2_title = f'{book2.title}'
@@ -24,16 +29,32 @@ class BookTestCase(TestCase):
 
     # VIEWS
     def test_get_books_route(self):
-        c = Client(enforce_csrf_checks=True)
-        response = c.get('/books/')
+        response = self.c.get('/books/')
         self.assertEquals(response.status_code, 200)
 
     def test_get_books_create_route(self):
-        c = Client()
-        response = c.get('/books/create/')
+        response = self.c.get('/books/create/')
         self.assertEquals(response.status_code, 200)
 
     def test_get_books_delete_route(self):
-        c = Client()
-        response = c.get('/books/delete/')
+        response = self.c.get('/books/delete/')
         self.assertEquals(response.status_code, 200)
+
+    def test_get_books_update_route(self):
+        response = self.c.get('/books/update/')
+        self.assertEquals(response.status_code, 200)
+
+    def test_index_to_add_books_route(self):
+        self.driver.get('http://127.0.0.1:8000/books/')        
+        self.driver.find_element_by_id('addBook').click()
+        self.assertIn('127.0.0.1:8000/books/create/', self.driver.current_url)
+
+    def test_index_to_delete_books_route(self):
+        self.driver.get('http://127.0.0.1:8000/books/')        
+        self.driver.find_element_by_id('deleteBook').click()
+        self.assertIn('127.0.0.1:8000/books/delete/', self.driver.current_url)
+
+    def test_index_to_update_books_route(self):
+        self.driver.get('http://127.0.0.1:8000/books/')        
+        self.driver.find_element_by_id('updateBook').click()
+        self.assertIn('127.0.0.1:8000/books/update/', self.driver.current_url)
--- books/views.py
+++ books/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from .forms import BookForm
+from .forms import CreateBookForm
 
 from .models import Book
 
@@ -7,7 +7,7 @@ from .models import Book
 def create(request):
     """ Create a book to add to a user's book list. """
     if request.method == 'POST':
-        form = BookForm(request.POST)
+        form = CreateBookForm(request.POST)
         if form.is_valid():
             try:
                 # Create Book object to store in db.
@@ -16,18 +16,16 @@ def create(request):
                 book.author = form.cleaned_data["author"]
                 book.genre = form.cleaned_data["genre"]
 
-                print('book title: ' + book.title)
-
                 # Save the new Book object in the db.
                 book.save()
                 return redirect('')
             except:
                 pass
         else:
-            form = BookForm()
+            form = CreateBookForm()
         return render(request, 'books/create.html', {'form': form})
     else:
-        form = BookForm()
+        form = CreateBookForm()
         return render(request, 'books/create.html', {'form': form})
 
 def index(request):
@@ -37,29 +35,36 @@ def index(request):
     }
     return render(request, 'books/index.html', reading_list)
 
-# def update(request, id):
-#     """ Edit details of a user's book in their book list. """
-#     book = Book.objects.get(id=id)
-#     return render(request, 'templates/edit.html', {'book': book})
+def update(request):
+    """ Edit details of a user's book in their book list. """
+    if request.method == 'POST':
+        # Get updated book data from form
+        book_selection = request.POST.get('book_selection')        
+        book = Book.objects.get(id=book_selection)        
+        new_title = request.POST.get('title')
+        new_author = request.POST.get('author')
+        new_genre = request.POST.get('genre')        
+
+        # Update book information in database
+        Book.objects.filter(title=book.title, author=book.author, genre=book.genre).update(title=new_title, author=new_author, genre=new_genre)         
+        return redirect('index')
+    else:
+        books = Book.objects.all()
+        return render(request, 'books/update.html', {'books': books})
 
 def delete(request):
     """ Remove a book from a user's book list. """
     if request.method == 'POST':
-        print('IN DELETE')
         try:
-            print('IN TRY')
             # Get array of books to remove.
             books = request.POST.getlist('checks[]')
 
             # Delete each book in array.
-            print('length = ' + str(len(books)))
             for book in books:
-                print('IN FOR LOOP')
                 book = book.replace(' - ', ' ').split(' ')
-                title, author, genre = book[0], book[1], book[2]                
+                title, author, genre = book[0], book[1], book[2]
                 Book.objects.filter(title=title, author=author,genre=genre).delete()
         except Exception as e:
-            print(e)
             pass
         return redirect('index')
     else:
