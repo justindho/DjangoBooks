2dc3ae3115cc55b93da2d1daf36f12b6671b08a7
Justin Ho <justinho515@gmail.com>
2019-07-09 20:59:43 -0700

    Fix import

--- books/forms.py
+++ books/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 from .models import Book
 
-class BookForm(forms.ModelForm):
+class CreateBookForm(forms.ModelForm):
     class Meta:
         model = Book
         fields = "__all__"
